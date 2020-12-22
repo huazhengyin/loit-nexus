2196f3ecd32b7c4b21359f9610e0fd22a91df575
huazhengyin <2871862077@qq.com>
2020-12-22 15:13:47 +0800

    添加项目

---
+++ src/main/java/com/loit/NexusTest.java
@@ -0,0 +1,13 @@
+package com.loit;
+
+import org.springframework.boot.SpringApplication;
+
+
+
+public class NexusTest {
+
+
+	/*public static final String NEXUS="===www.jincou.com===";*/
+
+
+}
---
+++ src/test/java/com/loit/test/hzy/com/LoitNexusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loit.test.hzy.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoitNexusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
